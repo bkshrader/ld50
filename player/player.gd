0d5afd4be98317071075b5038d972df5a28cc48e
Bradley <bradleykshrader@gmail.com>
2022-04-03 00:42:24 -0400

    Change player to rigid body

--- player/player.gd
+++ player/player.gd
@@ -1,38 +1,69 @@
 class_name Player
-extends KinematicBody2D
+extends RigidBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity") * ProjectSettings.get_setting("physics/2d/default_gravity_vector")
-var velocity = Vector2.ZERO
-var input = Vector2.ZERO
+# Member Variables
+export var jump_speed = 200.0
+export var movement_speed = 200.0
+export var max_jump_duration = 0.3
+export var jump_grace_duration = 0.1
+
+var is_on_floor = false
+
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# func _ready():
+# 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	input *= 0
-	if Input.is_action_pressed("player_right"):
-		input.x += 1
-	if Input.is_action_pressed("player_left"):
-		input.x -= 1
+# func _process(_delta):
+# 	pass
+
+
+func _integrate_forces(state):
+	# Check if player is on floor
+	var was_on_floor = is_on_floor
+	is_on_floor = false
+	for i in range(state.get_contact_count()):
+		if state.get_contact_local_normal(i).angle_to(Vector2.UP) <= PI/4:
+			is_on_floor = true
+			break
+	
+	# Handle Jumping
 	if Input.is_action_pressed("player_jump"):
-		input.y -= 20
-	if Input.is_action_pressed("player_crouch"):
-		input.y += 10
+		var grounded = is_on_floor or $GraceTimer.time_left > 0
+		var can_jump = grounded or $JumpTimer.time_left > 0
+		if can_jump:
+			set_axis_velocity(jump_speed * Vector2.UP)
+			if $JumpTimer.is_stopped():
+				$JumpTimer.start(max_jump_duration)
+				$GraceTimer.stop()
+	elif was_on_floor and not is_on_floor: # Player fell off ledge
+		$GraceTimer.start(jump_grace_duration)
+	
+	if Input.is_action_just_released("player_jump"):
+		$JumpTimer.stop()
+		$GraceTimer.stop()
 
+	# Handle Movement
+	var movement = 0
+	if Input.is_action_pressed("player_left"):
+		movement -= 1
+	if Input.is_action_pressed("player_right"):
+		movement += 1
 
-func _physics_process(delta):
-	self.velocity += GRAVITY * delta
-	if not is_on_floor():
-		input.y = clamp(input.y, 0, INF)
-	self.velocity = move_and_slide(velocity + 10 * input, Vector2.UP, true)
+	if movement != 0:
+		set_axis_velocity(movement_speed * movement * Vector2.RIGHT)
 
 
 func destroy():
 	print("Player Destroyed: Game Over")
+
+
+func _on_Player_body_entered(_body):
+	pass
+
+func _on_Player_body_exited(body):
+	var bodies = get_colliding_bodies()
+	bodies.erase(body)
