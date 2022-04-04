d7ad49b2e48acd9350f9cadb8f2ae50ef9692847
Bradley <bradleykshrader@gmail.com>
2022-04-04 01:05:01 -0400

    Add obstacle door and pressure plate

---
+++ button/PressurePlate.gd
@@ -0,0 +1,52 @@
+tool
+extends Node2D
+
+signal button_pressed
+signal button_released
+
+
+export var pressed = false setget set_pressed, is_pressed
+export var press_duration = 0.4
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func set_pressed(press):
+	$Tween.remove_all()
+	if press and not pressed:
+		# Tween press down
+		$Tween.interpolate_property($Button, 'position', $ButtonUp.position, $ButtonDown.position,
+		press_duration, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+		emit_signal("button_pressed")
+	if not press and pressed:
+		$Tween.interpolate_property($Button, 'position', $ButtonDown.position, $ButtonUp.position,
+		press_duration, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+		emit_signal("button_released")
+	
+	$Tween.start()
+	
+	pressed = press
+
+func is_pressed():
+	return pressed
+
+
+func _on_Area2D_body_entered(_body):
+	if not Engine.editor_hint:
+		set_pressed(true)
+
+
+func _on_Area2D_body_exited(_body):
+	$ReleaseDelay.start()
+
+
+func _on_ReleaseDelay_timeout():
+	if not $Collision.get_overlapping_bodies():
+		set_pressed(false)
---
+++ door/Door.gd
@@ -0,0 +1,60 @@
+tool
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var opened = false setget set_opened, is_opened
+export var open_duration = 0.5
+export var trigger: NodePath
+
+const REGION_OPENED = Rect2(0, 128, 32, 0)
+const REGION_CLOSED = Rect2(0, 128, 32, 32)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var trigger_node = get_node(trigger)
+	if trigger_node:
+		trigger_node.connect('button_released', self, 'close')
+		trigger_node.connect('button_pressed', self, 'open')
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func set_opened(open):
+	$Tween.remove_all()
+	if open and not self.opened:
+		# Tween door opening
+		$Tween.interpolate_property($Top, "region_rect", REGION_CLOSED, REGION_OPENED,
+		 open_duration, Tween.TRANS_EXPO, Tween.EASE_OUT)
+		$Tween.interpolate_property($Bottom, "region_rect", REGION_CLOSED, REGION_OPENED,
+		 open_duration, Tween.TRANS_EXPO, Tween.EASE_OUT)
+	elif not open and self.opened:
+		# Tween door closing
+		$Tween.interpolate_property($Top, "region_rect", REGION_OPENED, REGION_CLOSED,
+		 open_duration, Tween.TRANS_EXPO, Tween.EASE_IN)
+		$Tween.interpolate_property($Bottom, "region_rect", REGION_OPENED, REGION_CLOSED,
+		 open_duration, Tween.TRANS_EXPO, Tween.EASE_IN)
+		
+	$Tween.interpolate_callback($Collider/CollisionShape2D, open_duration, 'set_disabled', open)
+	$Tween.start()
+	
+	opened = open
+
+
+func is_opened():
+	return opened
+
+
+func open():
+	set_opened(true)
+
+
+func close():
+	set_opened(false)
