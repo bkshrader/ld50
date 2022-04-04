5702d95863f416e931d802ffcef0c4e208a486b9
Bradley <bradleykshrader@gmail.com>
2022-04-04 05:42:28 -0400

    Add automatic level sequencing

--- Main.gd
+++ Main.gd
@@ -4,13 +4,14 @@ extends Node2D
 # Declare member variables here. Examples:
 var history = ["res://ui/MainMenu.tscn"]
 
+var regex = RegEx.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var main_menu = get_child(0)
 	main_menu.connect('load_scene', self, 'on_load_scene')
-	# var test = test_scene.instance()
-	# test.set_name("Level")
-	# add_child(test)
+	
+	regex.compile("(\\d+)\\.tscn$")
 
 
 func remove_children():
@@ -34,6 +35,9 @@ func on_load_scene(scene):
 	
 	if scene_instance.has_signal('previous_scene'):
 		scene_instance.connect('previous_scene', self, 'on_previous_scene')
+		
+	if scene_instance.has_signal('next_scene'):
+		scene_instance.connect('next_scene', self, 'on_next_scene')
 
 
 func on_reload_scene():
@@ -43,3 +47,15 @@ func on_reload_scene():
 func on_previous_scene():
 	history.pop_front()
 	on_load_scene(history[0])
+
+
+func on_next_scene():
+	var result = regex.search(history[0])
+	if result:
+		var next_index = int(result.get_string(1)) + 1
+		if next_index <= 10:
+			var next_scene = 'res://levels/Level%d.tscn' % next_index
+			on_load_scene(next_scene)
+		else:
+			on_load_scene(history.back())
+
