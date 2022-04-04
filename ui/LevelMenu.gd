71f01dde082a4034c3cf9c3a7b79d1b53ccc52d1
Bradley <bradleykshrader@gmail.com>
2022-04-04 04:57:05 -0400

    Add all current levels to level select menu

--- ui/LevelMenu.gd
+++ ui/LevelMenu.gd
@@ -3,9 +3,10 @@ extends Node
 signal load_scene(scene)
 signal previous_scene
 
-func _on_Button_pressed():
+
+func _on_level_selected(level):
+	emit_signal('load_scene', 'res://levels/level%d.tscn' % level)
+
+
+func _on_back_button_pressed():
 	emit_signal('previous_scene')
-
-
-func _on_Level1_pressed():
-	emit_signal('load_scene', 'res://levels/level1.tscn')
--- ui/MainMenu.gd
+++ ui/MainMenu.gd
@@ -3,7 +3,7 @@ extends Node
 signal load_scene(scene)
 
 func _on_NewGame_pressed():
-	emit_signal('load_scene', 'res://levels/test.tscn')
+	emit_signal('load_scene', 'res://levels/Level1.tscn')
 
 func _on_Levels_pressed():
 	emit_signal('load_scene', 'res://ui/LevelMenu.tscn')
